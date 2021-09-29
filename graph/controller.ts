ba4cddb7cc3b032f554927abb746c8afdf6eb53b
Wharley01 <hackerslord96@gmail.com>
2021-09-29 20:08:33 +0100

    Added base controller

---
+++ graph/controller.ts
@@ -0,0 +1,24 @@
+import Request from "../request";
+import Response from "../response";
+import { Model } from "avanda-orm";
+import { Sequelize } from "sequelize";
+
+type AllowedMethods = 'any' | 'get' | 'post' | 'option'
+export default abstract class Controller{
+    model?: Model
+    connection: Sequelize
+    constructor(connection: Sequelize,model: Model | null = null, autolink: boolean =  false) {
+        this.connection = connection;
+        if (model){
+            this.model = model;
+            this.model.setModelName(this.model.constructor.name)
+        }
+
+    }
+    async get(response: Response,request: Request): Promise<any> {
+        return (await this.model?.first())
+    }
+    async getAll(response: Response,request: Request) {
+        return (await this.model?.all())
+    }
+}
